d6bf2f3b31d0769373f98c3bd263860a6bd842fc
Chris Gruel <chris.gruel@armory.io>
2021-05-13 18:04:45 -0400

    first pass at colorized all command display

--- src/commands/all.mjs
+++ src/commands/all.mjs
@@ -1,7 +1,7 @@
 import shelljs from 'shelljs';
 import kleur from "kleur";
 const { which, exec } = shelljs;
-const regex = /^(?<filePath>.+):(?<lineNumber>\d+):(?<columnNumber>\d+) - .+\((?<misspelledWord>\w+)\)/gm;
+const spellingErrorRegex = /^(?<filePath>.+):(?<lineNumber>\d+):(?<columnNumber>\d+)\s+-\s+.+\((?<misspelledWord>\w+)\)(?:\s+--)?\s?(?<wordContext>.+)?/gm;
 
 export const command = "all";
 export const describe = "perform full spell check of all rpy files";
@@ -11,6 +11,57 @@ export async function handler (argv){
     if(cSpellWhich?.code !== 0){
         console.log(kleur.red('A global installation of cspell is required! Install using \"npm install -g cspell\"'));
     } else {
-        exec('cspell --show-context --no-progress "**/*.rpy"', {silent:false}).stdout;
+        // should we suppress intro
+        if (!argv.s) {
+            console.log(kleur.yellow('Checking all files for spelling errors...'));
+        }
+        let spellingResults = exec(`cspell --show-context --no-progress --no-summary "**/*.rpy"`, {silent:true}).stdout;
+        let matches = [];
+        if (spellingResults) {
+            matches = getMatches(spellingResults, spellingErrorRegex, matches);
+            let yellowColon = kleur.yellow(":");
+            let fileLengthList = matches.map(item => `${kleur.green(item?.groups?.filePath)}${yellowColon}${kleur.yellow(item?.groups?.lineNumber)}${yellowColon}${kleur.yellow(item?.groups?.columnNumber)}`?.length || 0);
+            // console.log('fileLengthList', JSON.stringify(fileLengthList,null,2));
+            let maxFileLength = Math.max(...fileLengthList);
+            let paddedFilePathLength = maxFileLength;
+            // console.log('paddedFilePathLength', paddedFilePathLength);
+            let matchesGroups = matches.map(item => item?.groups);
+            // console.log('matchesGroups', JSON.stringify(matchesGroups,null,2));
+            let wordLengthList = matches.map(item => `   - Unknown word (${kleur.red(item?.groups?.misspelledWord)})`.length);
+            let maxWordLength = Math.max(...wordLengthList);
+            for (const match of matches) {
+                let path = match?.groups?.filePath;
+                let lineNumber = match?.groups?.lineNumber;
+                let word = match?.groups?.misspelledWord;
+                let columnNumber = match?.groups?.columnNumber;
+                let wordContext = match.groups?.wordContext ?? '';
+                let pathWIthLinesAndCols = `${kleur.green(path)}${yellowColon}${kleur.yellow(lineNumber)}${yellowColon}${kleur.yellow(columnNumber)}`;
+                let paddedPathWIthLinesAndCols = pathWIthLinesAndCols.padEnd(paddedFilePathLength, ' ');
+                let misspelledWord = `   - Unknown word (${kleur.red(word)})`;
+                let paddedWord = misspelledWord.padEnd(maxWordLength, ' ');
+                let highlightedWordContext = '';
+                // console.log('wordContext', wordContext);
+                if (wordContext) {
+                    let wordContextArray = wordContext.split(word);
+                    highlightedWordContext = `    -- ${kleur.gray().gray(wordContextArray.join(kleur.red(word)))}`;
+                }
+                console.log(`${paddedPathWIthLinesAndCols}${paddedWord}${highlightedWordContext}`);
+            }
+        } else {
+            console.log(kleur.green('No misspelled words detected!'));
+        }
     }
+}
+
+function getMatches(stringToSearch, regexToSearchWith, matchArray = []){
+    let matches = matchArray;
+    let m;
+    while ((m = regexToSearchWith.exec(stringToSearch)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === regexToSearchWith.lastIndex) {
+            regexToSearchWith.lastIndex++;
+        }
+        matches.push(m);
+    }
+    return matches;
 }
