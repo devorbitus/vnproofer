cb7274557cb275add62ba7f126906bba0ea1a74d
Chris Gruel <chris.gruel@armory.io>
2021-05-14 17:38:09 -0400

    more DRY consolidation

--- src/commands/all.mjs
+++ src/commands/all.mjs
@@ -1,21 +1,18 @@
 import shelljs from 'shelljs';
 import kleur from "kleur";
-import { handleSpellingResults, printSpellingLines } from '../utils/util.mjs';
+import { handleSpellingResults, printSpellingLines, cSpellExistsChecker } from '../utils/util.mjs';
 const { which, exec } = shelljs;
 
 export const command = "all";
 export const describe = "perform full spell check of all rpy files";
 export const aliases = ['a'];
 export async function handler(argv) {
-    var cSpellWhich = which('cspell');
-    if (cSpellWhich?.code !== 0) {
-        console.log(kleur.red('A global installation of cspell is required! Install using \"npm install -g cspell\"'));
-    } else {
+    cSpellExistsChecker(function cSpellExistsCheckerAllCommandSuccess(){
         // should we suppress intro
         if (!argv.suppressIntro) {
             console.log(kleur.yellow('Checking all files for spelling errors...'));
         }
         const spellingResults = exec(`cspell --show-context --no-progress --no-summary "**/*.rpy"`, { silent: true }).stdout;
         printSpellingLines(handleSpellingResults(spellingResults));
-    }
+    });
 }
--- src/commands/check.mjs
+++ src/commands/check.mjs
@@ -2,32 +2,38 @@ import kleur from "kleur";
 import fs from "fs";
 import shelljs from 'shelljs';
 const { exec } = shelljs;
-import { handleSpellingResults, printSpellingLines } from '../utils/util.mjs';
+import { handleSpellingResults, printSpellingLines, cSpellExistsChecker } from '../utils/util.mjs';
 
 export const command = "check <file>";
 export const describe = "check file for spelling errors";
-export function builder (yargs) {
+export function builder(yargs) {
     yargs.positional('file', {
         describe: "file to check for spelling errors",
         type: 'string'
     });
 };
 export const aliases = ['c'];
-export function validation (foo, bar) {
+export function validation(foo, bar) {
     console.log('foo', foo);
     console.log('bar', bar);
 }
-export async function handler (argv) {
+export async function handler(argv) {
     if (!argv.file) {
         console.log(kleur.red('Please provide a file path.'))
         process.exit(1);
     }
 
-    if (!fs.existsSync(argv.file) || argv.file === "." || argv.file ==="..") {
-        // console.log('argv', JSON.stringify(argv,null,2))
+    if (!fs.existsSync(argv.file) || argv.file === "." || argv.file === "..") {
+        console.log('argv', JSON.stringify(argv,null,2))
         console.log(kleur.red(`File [ ${argv.file} ] does not exist.`))
         process.exit(1)
     }
-    const spellingResults = exec(`cspell --show-context --no-progress --no-summary ${argv.file}`, { silent: true }).stdout;
-    printSpellingLines(handleSpellingResults(spellingResults));
+    cSpellExistsChecker(function cSpellExistsCheckerCheckCommandSuccess(){
+        // should we suppress intro
+        if (!argv.suppressIntro) {
+            console.log(kleur.yellow(`Checking file [ ${argv.file} ] for spelling errors...`));
+        }
+        const spellingResults = exec(`cspell --show-context --no-progress --no-summary ${argv.file}`, { silent: true }).stdout;
+        printSpellingLines(handleSpellingResults(spellingResults));
+    });
 }
