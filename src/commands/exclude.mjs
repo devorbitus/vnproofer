b21554a21c987d97ca714ae06f83c9446c53b934
Chris Gruel <chris.gruel@armory.io>
2021-05-19 10:28:18 -0400

    example docs for exclude

--- src/commands/exclude.mjs
+++ src/commands/exclude.mjs
@@ -8,9 +8,12 @@ export function builder(yargs) {
     describe: "File path that has the misspelled word",
     type: 'string'
   }).positional('word', {
-    describe: 'Word to exclude from misspelled words within file',
+    describe: 'Word or words to exclude from misspelled words within file',
     type: 'string'
-  });
+  }).example([
+    ['$0 exclude ./myfile.rpy fixin', 'Add a single word to the file\'s word exclusion list'],
+    ['$0 e ./myfile.rpy "fixin yall ain\'t"', 'Add multiple space-seperated words to the file\'s word exclusion list']
+  ]);
 };
 export async function handler(argv) {
   checkFileExists(argv.file);
